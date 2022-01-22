0b550e2b4d882a4adba641eb57d6c318d2fb07f6
kamalMohamed <kmohamed@turndigital.net>
2022-01-22 07:24:15 +0200

    add pseudo code steps

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("hello from server")
+// pseudo code
+//#region stage 1 image processing
+// 1- add image processing route
+// 2- on get request validate request params (size) >> if not add default size
+// 3- check file name exist and not processed before
+// 4- if not exist return not exist message
+// 5- if processed before return processed image
+// 6- if processed before process image then return it 
+//#endregion
+
+//#region stage 2 add public static route
+// if there is time
+//#endregion
